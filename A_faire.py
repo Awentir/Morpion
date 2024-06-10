08277a7f4d4b61faf30969587626e2b21e2afe0c
Thomas Corduan <thomas.corduan@gmail.com>
2024-06-10 14:17:59 +0200

    A faire 3 ended

---
+++ A_faire.py
@@ -0,0 +1,11 @@
+import pygame
+
+surf=pygame.display.set_mode((800,600))
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run = False
+    pygame.draw.line(surf,(255,255,255),(10,20),(150,200),2)
+    pygame.display.flip()
+pygame.quit()
